068349f4308d928e39e8ec6a0a06a4cd20b434a0
robdinges <robdinges@hotmail.com>
2020-10-12 19:31:00 +0200

    initial version

---
+++ best.R
@@ -0,0 +1,20 @@
+
+
+best <- function(state, outcomename) {
+        # outcomename = “heart attack” (11), “heart failure” (17), 
+        # or “pneumonia” (23)
+        # exclude hospitals that have no data
+        # use hospital name from field Hospital.Name
+        
+        
+        ## Read outcome data
+        
+        ## Check that state and outcome are valid
+        
+        ## Return hospital name in that state with lowest 30-day death
+        
+        ## rate
+        
+        
+        
+}
